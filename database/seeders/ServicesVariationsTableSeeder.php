bfac1adf0ca1e6290b1b8c90248129050c778405
vansilich <vunssun@gmail.com>
2024-12-04 21:53:18 +0300

    up prices

--- database/seeders/ServicesVariationsTableSeeder.php
+++ database/seeders/ServicesVariationsTableSeeder.php
@@ -17,75 +17,75 @@ class ServicesVariationsTableSeeder extends Seeder
         DB::table('services_variations')->insert([
             'service_id' => 1,
             'duration' => 60,
-            'price' => 4000,
+            'price' => 5000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232674',
         ]);
         DB::table('services_variations')->insert([
             'service_id' => 1,
             'duration' => 90,
-            'price' => 5000,
+            'price' => 6000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232729',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 2,
             'duration' => 60,
-            'price' => 3000,
+            'price' => 4000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9216228'
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 3,
             'duration' => 60,
-            'price' => 3000,
+            'price' => 4000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s12075751',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 4,
             'duration' => 60,
-            'price' => 4000,
+            'price' => 5000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232736',
         ]);
         DB::table('services_variations')->insert([
             'service_id' => 4,
             'duration' => 90,
-            'price' => 5000,
+            'price' => 6000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232743',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 5,
             'duration' => 40,
-            'price' => 3000,
+            'price' => 4000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232747',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 6,
             'duration' => 40,
-            'price' => 3000,
+            'price' => 4000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232749',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 7,
             'duration' => 15,
-            'price' => 1000,
+            'price' => 2000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232755',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 8,
             'duration' => 20,
-            'price' => 1000,
+            'price' => 2000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s9232761',
         ]);
 
         DB::table('services_variations')->insert([
             'service_id' => 9,
             'duration' => 30,
-            'price' => 3000,
+            'price' => 4000,
             'link' => 'https://b689665.yclients.com/company/651678/menu?o=m1860233s13337856',
         ]);
     }
