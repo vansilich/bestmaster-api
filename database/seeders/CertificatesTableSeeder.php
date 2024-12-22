713a7a7a8513b1e4a996ffdf089d4f7723e3069e
vansilich <vunssun@gmail.com>
2024-12-22 23:50:17 +0300

    up certs prices

--- database/seeders/CertificatesTableSeeder.php
+++ database/seeders/CertificatesTableSeeder.php
@@ -16,17 +16,17 @@ class CertificatesTableSeeder extends Seeder
     {
         DB::table('certificates')->insert([
             'title' => 'Восстановительный массаж спины (1ч)',
-            'price' => 3000,
+            'price' => 4000,
             'photo' => 'certificate_1.png'
         ]);
         DB::table('certificates')->insert([
             'title' => 'Восстановительный массаж всего тела (1ч)',
-            'price' => 4000,
+            'price' => 5000,
             'photo' => 'certificate_2.png'
         ]);
         DB::table('certificates')->insert([
             'title' => 'Общий массаж всего тела (1,5ч)',
-            'price' => 5000,
+            'price' => 6000,
             'photo' => 'certificate_3.png'
         ]);
     }
